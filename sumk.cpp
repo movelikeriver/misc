a336bd7a32190c23886901d9bedbcf31d14ab627
staylikemount <staylikemount@users.noreply.github.com>
2015-07-27 10:00:36 -0700

    fix a segment false in sumk.cpp
    use copy instead of pointer in recursion

--- sumk.cpp
+++ sumk.cpp
@@ -35,8 +35,7 @@ bool Sum2(const list<int>& inputs, int value, int* ii, int* jj) {
 }
 
 void TestSum2() {
-  //  list<int> inputs = {1, 2, 3, 5, 8, 10, 12, 16, 18};
-  list<int> inputs = {3, 5, 8, 10, 12, 16, 18};
+  list<int> inputs = {1, 2, 3, 5, 8, 10, 12, 16, 18};
   PRINT(inputs);
 
   int ii = 0, jj = 0;
@@ -50,13 +49,23 @@ void TestSum2() {
   }
 }
 
-bool SumK(list<int>* inputs, int value, int k, vector<int>* results) {
+void CopyList(const list<int>& input, list<int>* output,
+	      list<int>::const_iterator except) {
+  for (auto it = input.begin(); it != input.end(); ++it) {
+    if (it != except) {
+      output->emplace_back(*it);
+    }
+  }
+}
+
+// use vector<int> is also fine.
+bool SumK(const list<int>& inputs, int value, int k, vector<int>* results) {
   cout << "sum k=" << k << ", value=" << value << endl;
-  PRINT(*inputs);
+  PRINT(inputs);
 
   if (k == 2) {
     int ii=0, jj=0;
-    if (Sum2(*inputs, value, &ii, &jj)) {
+    if (Sum2(inputs, value, &ii, &jj)) {
       cout << "find1: " << ii << ", " << jj << endl;
 
       results->emplace_back(ii);
@@ -67,16 +76,17 @@ bool SumK(list<int>* inputs, int value, int k, vector<int>* results) {
     return false;
   }
 
-  for (auto it = inputs->begin(); it != inputs->end(); it++) {
+  list<int> tmp = inputs;  // Copy once for each k.
+  for (auto it = tmp.begin(); it != tmp.end(); it++) {
     int v = *it;
-    // erase and emplace can recover the original list.
-    auto it2 = inputs->erase(it);
     results->emplace_back(v);
-    bool found = SumK(inputs, value - v, k - 1, results);
 
-    cout << "==begin==";
-    it = inputs->emplace(it2, v);
-    cout << "==end==";
+    // Doing erase and emplace can recover the original list, but it
+    // would trigger segment fault in nested mutable list due to
+    // invalid pointer.
+    auto it2 = tmp.erase(it);
+    bool found = SumK(tmp, value - v, k - 1, results);
+    it = tmp.emplace(it2, v);
 
     if (found) {
       return true;
@@ -94,7 +104,7 @@ void TestSumK(int k) {
   vector<int> values = {11, 20, 15, 18, 34, 33};
   for (const int value : values) {
     vector<int> results;
-    if (SumK(&inputs, value, k, &results)) {
+    if (SumK(inputs, value, k, &results)) {
       cout << "find " << value << ": ";
       PRINT(results);
     } else {
@@ -104,7 +114,7 @@ void TestSumK(int k) {
 }
 
 int main() {
-  //  TestSum2();
-  //  TestSumK(3);
+  TestSum2();
+  TestSumK(3);
   TestSumK(4);
 }
